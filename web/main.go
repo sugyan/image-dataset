90eab27bf198434e88d4ba8e7521b170def360c3
sugyan <sugi1982@gmail.com>
2019-09-18 00:59:45 +0900

    Initial commit

---
+++ web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
