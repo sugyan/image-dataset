f749ae2176bedc08f88365eccd0a30f948ebdbf7
sugyan <sugi1982@gmail.com>
2019-09-20 01:28:02 +0900

    Update frontend

---
+++ frontend/src/components/Index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Index: React.FC = () => {
+    return (
+        <div>
+            <header className="App-header">
+                <Link
+                    className="App-link"
+                    to="/signin"
+                >
+                  Sign in
+                </Link>
+            </header>
+        </div>
+    );
+};
+
+export default Index;
---
+++ frontend/src/components/Signin.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Signin: React.FC = () => {
+    return (
+        <div>Sign in</div>
+    );
+};
+
+export default Signin;
